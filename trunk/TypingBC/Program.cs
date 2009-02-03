ef94284a5b1c8e001f1564f455d4937691f52a4d
nguyenvinhtiep <nguyenvinhtiep@25f6d9c8-ed1a-11dd-a550-c997509ed985>
2009-02-03 07:32:56 +0000

    To: Cac bac Hom nay T phai di qua nha em ut. Toi moi ve lam tiep. To: Blackpawn Long hien thuc hoa them mot so phan nhu ShowLayerXXX() dum T nha!

--- trunk/TypingBC/Program.cs
+++ trunk/TypingBC/Program.cs
@@ -14,7 +14,7 @@ namespace TypingBC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Presentation.View.Form1());
+            Application.Run(new Presentation.View.ViewDlg());
         }
     }
 }
